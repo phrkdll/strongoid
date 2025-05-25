3e599a200ab79a2f193e8603b9f77eb25f196a82
Benjamin <benjamin.hotop@gmail.com>
2025-05-26 00:01:16 +0200

    add json un-/marshal

---
+++ strongoid_json.go
@@ -0,0 +1,19 @@
+package strongoid
+
+import "encoding/json"
+
+func (id *Id[T]) MarshalJSON() ([]byte, error) {
+	return json.Marshal(id.Inner)
+}
+
+func (id *Id[T]) UnmarshalJSON(data []byte) error {
+	var value T
+
+	if err := json.Unmarshal(data, &value); err != nil {
+		return err
+	}
+
+	id.Inner = value
+
+	return nil
+}
